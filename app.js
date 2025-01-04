612b297128cf2638ea5d7aeebb7ab6abeec0ed70
sdpriyaram <sdpriyaramesh@gmail.com>
2025-01-04 15:57:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+// Set the 'public' folder as static
+app.use(express.static(path.join(__dirname, 'public')));
+
+//comment
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile('/index.html');
+});
+
+app.use(express.static('./public'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
---
+++ src/employees.jsx
@@ -0,0 +1,18 @@
+let sally = 'Sally Smith'
+let mark = 'Mark Martin'
+let holly = 'Holly Unlikely'
+let amol = 'Amol Shookup'
+let robin = 'Robin Bainks'
+let john = 'John Doe'
+const element = (
+    <ul style={{'color':'blue', 'fontSize': '24px'}}>
+        <li>{sally}</li>
+        <li>{mark}</li>
+        <li>{holly}</li>
+        <li>{amol}</li>
+        <li>{robin}</li>
+        <li>{john}</li>
+    </ul>
+)
+ReactDOM.render(element, document.getElementById('content'))
+
